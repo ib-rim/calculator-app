0ff7912575549c9f5ae2b2af4daf060443b529c0
ib-rim <ib.bashir@hotmail.com>
2021-06-02 22:09:23 +0100

    Updated readme and added theme saving using localstorage

--- README.md
+++ README.md
@@ -10,8 +10,6 @@ This is a solution to the [Calculator app challenge on Frontend Mentor](https://
   - [Links](#links)
 - [Author](#author)
 
-**Note: Delete this note and update the table of contents based on what sections you keep.**
-
 ## Overview
 
 ### The challenge
--- calculator.js
+++ calculator.js
@@ -1,27 +1,55 @@
 
-var theme = 1;
+//theme functionality
+
+setCurrentTheme()
+
+function setCurrentTheme() {
+    if (localStorage.getItem('theme') === '1'){
+        setTheme1();
+    } 
+    else if (localStorage.getItem('theme') === '2'){
+        setTheme2();
+    } 
+    if (localStorage.getItem('theme') === '3'){
+        setTheme3();
+    } 
+}
+
+function setTheme1() {
+    document.getElementById("slider").style.margin = "52px 0 0 240px";
+    document.getElementById("html").className = "theme1";
+    localStorage.setItem('theme', '1');
+}
+
+function setTheme2() {
+    document.getElementById("slider").style.margin = "52px 0 0 265px";
+    document.getElementById("html").className = "theme2";
+    localStorage.setItem('theme', '2');
+}
+
+function setTheme3() {
+    document.getElementById("slider").style.margin = "52px 0 0 290px";
+    document.getElementById("html").className = "theme3";
+    localStorage.setItem('theme', '3');
+}
+
+function toggleTheme() {
+    if(localStorage.getItem('theme')==='1'){
+        setTheme2();
+    }
+    else if(localStorage.getItem('theme')==='2'){
+        setTheme3();
+    }
+    else if(localStorage.getItem('theme')==='3'){
+        setTheme1();
+    }
+}
+
+//calculator functonality
 
 var wipe = false;
 var expression = "";
 var screenExpression = "";
-
-function toggleTheme() {
-    if(theme==1){
-    document.getElementById("slider").style.margin = "52px 0 0 265px";
-    document.getElementById("html").className = "theme2";
-    theme = 2;
-    }
-    else if(theme==2){
-    document.getElementById("slider").style.margin = "52px 0 0 290px";
-    document.getElementById("html").className = "theme3";
-    theme = 3;
-    }
-    else {
-    document.getElementById("slider").style.margin = "52px 0 0 240px";
-    document.getElementById("html").className = "theme1";
-    theme = 1;
-    }
-}
 
 function concat(val) {
     if(wipe==true){
